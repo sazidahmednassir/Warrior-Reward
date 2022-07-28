bfd113284e239ccecb5bafbee6364790bfd2ab72
sazidahmednassir <sazidahmednassir@gmail.com>
2022-07-28 22:41:49 +0600

    add circlefor mobileview

--- src/Home/FirstSection/FirstSection.js
+++ src/Home/FirstSection/FirstSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import trophy from '../../Images/Icons/tr.svg';
 import user from '../../Images/User/user.png';
 import './FirstSection.css';
 const FirstSection = () => {
@@ -11,7 +12,17 @@ const FirstSection = () => {
         <span className='userName'>ইশতিয়াক</span>
       <span className='userLevel'>Level-Warrior</span>
       </div>
-     
+      <div className='circle'>
+        
+      </div>
+     <div className='reward'>
+        <img src={trophy} alt="" />
+        
+
+        <div>
+            <span className='rewardText'>রিওয়ার্ড</span>
+            </div>
+     </div>
       
       </div>
     </div>
